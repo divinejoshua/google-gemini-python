e7c88d1e0a8e978ccdb0f8f1119d4e1d8683e75a
divinejoshua <divineuche399@gmail.com>
2025-03-27 01:18:30 +0000

    Added image editing

---
+++ image-editing.py
@@ -0,0 +1,29 @@
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+
+import PIL.Image
+from decouple import config
+
+image = PIL.Image.open('./original-image.jpg')
+
+client = genai.Client(api_key=config("GEMINI_API_KEY"))
+
+text_input = ('Hi, This is a picture of me.'
+            'Convert this photo to studio ghibli style anime?',)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-exp-image-generation",
+    contents=[text_input, image],
+    config=types.GenerateContentConfig(
+      response_modalities=['Text', 'Image']
+    )
+)
+
+for part in response.candidates[0].content.parts:
+  if part.text is not None:
+    print(part.text)
+  elif part.inline_data is not None:
+    image = Image.open(BytesIO(part.inline_data.data))
+    image.show()
