feb94b4ecd8a2da96eb234cd0d44b8207061f786
divinejoshua <divineuche399@gmail.com>
2025-06-13 19:44:49 +0100

    Added streaming

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from decouple import config
 
 client = genai.Client(api_key=config("GEMINI_API_KEY"))
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+response = client.models.generate_content_stream(
+    model="gemini-2.0-flash",
+    contents=["Explain how AI works"]
 )
-print(response.text)
+for chunk in response:
+    print(chunk.text, end="")
